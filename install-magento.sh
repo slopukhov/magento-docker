641430f222f476201e107163491bb65910b9ffc2
yaroslavgoncharuk <goncharu@adobe.com>
2022-11-02 13:57:06 -0500

    added mftf support

--- install-magento.sh
+++ install-magento.sh
@@ -8,6 +8,8 @@ export $(cut -s -d= -f1 ./.env)
 DOCKER_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DOCKER_PROJECT_DIR
+cp etc/php/tests/.env ../repos/magento2ce/dev/tests/acceptance
+cp etc/php/tests/.credentials ../repos/magento2ce/dev/tests/acceptance
 
 docker-compose exec app magento install
 
